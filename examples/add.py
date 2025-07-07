6baed0084d60adf17b20213f345e60a86e0660d3
KrispWaffle <ijwu345@gmail.com>
2025-07-06 22:24:30 -0700

    v.2 big update

--- examples/add.py
+++ examples/add.py
@@ -1,8 +1,31 @@
-from neuro.core import Tensor
-
-
-
-x = Tensor(1.0)
-y = Tensor(2.0)
-
-print(x+y)
+from neuro.core import *
+from neuro.graphs import print_graph
+import torch
+x = Tensor([[1., 2., 3.],
+    [4., 5., 6.]],requires_grad=True)
+y = Tensor([[7., 8.],
+    [9., 10.],
+    [11., 12.]], requires_grad=True)
+A = torch.tensor([
+    [1., 2., 3.],
+    [4., 5., 6.]
+], requires_grad=True)
+B = torch.tensor([
+    [7., 8.],
+    [9., 10.],
+    [11., 12.]
+], requires_grad=True)
+C = A @ B
+z= x@y
+loss = C.sum()
+loss2 = z.sum()
+loss2.backward()
+loss.backward()
+print("--- Gradients ---")
+print("Gradient with respect to A (A.grad):\n", A.grad)
+print("\nGradient with respect to B (B.grad):\n", B.grad)
+print("Gradient with respect to x (x.grad):\n", x.grad)
+print("\nGradient with respect to y (y.grad):\n", y.grad)
+print_graph(z)
+#print(x.grad)   # should be something sensible, e.g. [-0.25  1.  ]
+#print(y.grad)   # should be [3. 3.]
